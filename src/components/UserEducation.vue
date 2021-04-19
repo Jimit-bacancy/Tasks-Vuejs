d679d989df2322ac768e1b2f18b945e6de131de8
Jimit-bacancy <jimit.shah@bacancy.com>
2021-04-19 20:07:40 +0530

    Fixing getting data into datatable

--- src/components/UserEducation.vue
+++ src/components/UserEducation.vue
@@ -15,30 +15,69 @@
       </b-col> </b-row><br />
     <b-row>
       <b-col>
-        <b-table striped hover bordered :items="formdata" :filter="filter" 
+        <b-table striped hover bordered :items="totaldata" :filter="filter" show-empty stacked="md"
                 :per-page="perPage" :current-page="currentPage" :fields="fields"> 
             <template v-slot:cell(actions)="data">
                 <b-button variant="danger" @click="deleteItem(data.item.id)">Delete</b-button>
             </template>
         </b-table>
-        <b-pagination v-model="currentPage" :total-rows="rows" :per-page="perPage"></b-pagination>
+        <b-pagination v-model="currentPage" align="center" pills size="lg" :total-rows="rows" :per-page="perPage"></b-pagination>
       </b-col>
     </b-row>
   </b-container>
 </template>
 
 <script>
+import axios from 'axios';
+import { mapState } from 'vuex';
 export default {
-  props: ["formdata"],
+  // props: ["formdata"],
   data() {
     return {
       filter: "",
       perPage: 2,
+      key: "actions",
+      label: "Actions",
       currentPage: 1,
-      fields: ['id','fname','lname','institute','branch','cgpa','startdate','enddate','actions'],
+      // fields: ['id','fname','lname','institute','branch','cgpa','startdate','enddate','actions'],
+      fields: [
+        {
+          key: 'fname',
+          sortable: true,
+        },
+        {
+          key: 'lname',
+          sortable: true,
+        },
+        {
+          key: 'institute',
+          sortable: true,
+        },
+        {
+          key: 'stream',
+          sortable: true,
+        },
+        {
+          key: 'cgpa',
+          sortable: true,
+        },
+        {
+          key: 'startdate',
+          sortable: true,
+        },
+        {
+          key: 'enddate',
+          sortable: true,
+        },
+        {
+          key: 'actions',
+          sortable: true,
+        }
+      ],
     };
   },
   computed: {
+    ...mapState(['totaldata']),
     rows() {
       if (this.formdata) {
         return this.formdata.length;
@@ -48,10 +87,28 @@ export default {
     },
   },
   methods: {
-      deleteItem(id){
-        const index = this.formdata.indexOf((x) => x.id === id);
-        this.formdata.splice(index,1);
+      // deleteItem(id){
+      //   const index = this.formdata.indexOf((x) => x.id === id);
+      //   this.formdata.splice(index,1);
+      // }
+      deleteItem(id) {
+      if (confirm("Are you sure you want to delete this Data?")) {
+        axios
+          .delete(
+            `https://registration-form-e64d5-default-rtdb.firebaseio.com/formResponse/${id}.json`
+          )
+          .then((res) => {
+            console.log("Response Data = ", res.data);
+
+            const filteredUsers = this.totaldata.filter((item) => item.uid !== id);
+            this.$emit("update:posts", filteredUsers);
+            alert("data deleted");
+          });
       }
+    },
   },
+  mounted() {
+    console.log("Posts =", this.totaldata);
+  }
 };
 </script>
--- src/components/UserEducationData.vue
+++ src/components/UserEducationData.vue
@@ -1,6 +1,6 @@
 <template>
     <b-container>
-        <user-education :formdata="formdata"></user-education>
+        <user-education :totalata.sync="totaldata"></user-education>
     </b-container>
 </template>
 
@@ -12,39 +12,16 @@ export default {
     },
     data() {
         return {
-            formdata: [
-                {
-                    id: 1,
-                    fname: 'Abc',
-                    lname: 'Xyz',
-                    institute: 'GTU',
-                    branch: 'IT',
-                    cgpa: 8.45,
-                    startdate: '8-5-2016',
-                    enddate: '8-6-1019'
-                },
-                {
-                    id: 2,
-                    fname: 'Def',
-                    lname: 'Qwe',
-                    institute: 'GTU',
-                    branch: 'CE',
-                    cgpa: 8.78,
-                    startdate: '8-5-2017',
-                    enddate: '8-6-1020'
-                },
-                {
-                    id: 3,
-                    fname: 'Xyz',
-                    lname: 'Zxc',
-                    institute: 'GTU',
-                    branch: 'IT',
-                    cgpa: 8.97,
-                    startdate: '8-5-2016',
-                    enddate: '8-6-1019'
-                },
-            ]
+            totaldata: [],
         }
+    },
+    methods: {
+        getData(){
+            this.$store.dispatch('getFormData');
+        }
+    },
+    mounted(){
+        this.getData();
     },
 }
 </script>
